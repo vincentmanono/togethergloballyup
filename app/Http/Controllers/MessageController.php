2454f07f3dd5f453b30623b4481185d39cdbb388
vincent <vincentnyamongo95@gmail.com>
2020-07-14 21:52:40 +0300

    linux

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -35,11 +35,10 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-<<<<<<< HEAD
-       
-=======
+
+
         // $mails = Mail::
->>>>>>> e5e1826f4f0a314900773bcc795d381a960e9b92
+
     }
 
     /**
