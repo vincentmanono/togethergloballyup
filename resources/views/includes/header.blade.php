97fbd74be1959c3b87875cbcd53cf8a89a478c4d
Abrahamkivosh <abrahamkivosh@gmail.com>
2020-08-02 01:23:04 +0300

    Modified footer color

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -46,43 +46,47 @@
             </div>
         </div>
     </div> --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top  pt-4 pb-4 ">
+    <nav class="navbar navbar-expand-lg navbar-dark navbarcontainer fixed-top  pt-4 pb-4 ">
         <a class="navbar-brand" href="{{ route('index') }}">{{ config('app.name') }}</a>
-        <button class="navbar-toggler d-lg-none bg-dark " type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler d-lg-none bg-dark " type="button" data-toggle="collapse"
+            data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+            aria-label="Toggle navigation">
             <i class="fa fa-pencil" aria-hidden="true"> more </i>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item {{ (request()->is('/') ) ? 'active' : '' }} ">
-                    <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item {{ request()->is('/') ? 'active' : '' }} ">
+                    <a style="color: #fff" class="nav-link mynav " href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
                 </li>
 
-                <li   class="nav-item {{ (request()->is('about') ) ? 'active' : '' }} " ><a class="nav-link" href="/about ">About Us</a></li>
+                <li class="nav-item {{ request()->is('about') ? 'active' : '' }} ">
+                    <a   class="nav-link mynav "
+                        href="/about ">About Us</a></li>
 
-                <li   class="nav-item {{ (request()->is('services')  ) ? 'active' : '' }} " ><a  class="nav-link" href="/services">Services</a></li>
-                <li   class="nav-item {{ (request()->is('testimonial')  ) ? 'active' : '' }}" ><a  class="nav-link" href="/testimonial">Testimonials</a></li>
-                <li   class="nav-item {{ (request()->is( 'contact') ) ? 'active' : '' }} " ><a  class="nav-link" href="/contact">Contact</a></li>
-                <li   class="nav-item {{ (request()->is('faq') ) ? 'active' : '' }} " ><a  class="nav-link" href="/faq">FAQ</a></li>
-
-                {{-- <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
-                    </div>
-                </li> --}}
+                <li class="nav-item {{ request()->is('services') ? 'active' : '' }} ">
+                    <a   class="nav-link mynav "
+                        href="/services">Services</a></li>
+                <li class="nav-item {{ request()->is('testimonial') ? 'active' : '' }}">
+                    <a   class="nav-link mynav "
+                        href="/testimonial">Testimonials</a></li>
+                <li class="nav-item {{ request()->is('contact') ? 'active' : '' }} ">
+                    <a   class="nav-link mynav "
+                        href="/contact">Contact</a></li>
+                <li class="nav-item {{ request()->is('faq') ? 'active' : '' }} ">
+                    <a   class="nav-link mynav " href="/faq">FAQ</a>
+                </li>
             </ul>
-            <ul class=" credetials text text-info list-inline " >
-                <li  class="list-inline-item nav-item " >
-                    <a name="" id="" href="{{ route('login') }}" class="login nav-link "
-                        role="button">Login</a>
+            <ul class=" credetials text text-info list-inline ">
+                <li class="list-inline-item nav-item ">
+                    <a name="" id="" href="{{ route('login') }}" class="login nav-link " role="button">Login</a>
                 </li>
-                <li  class="list-inline-item nav-item " >
-                    <a name="" id="" href="{{ route('register') }}" class="signup nav-link "
-                        role="button">Signup</a>
+                <li class="list-inline-item nav-item ">
+                    <a name="" id="" href="{{ route('register') }}" class="signup nav-link " role="button">Signup</a>
                 </li>
             </ul>
         </div>
     </nav>
+
+
+
 </header>
