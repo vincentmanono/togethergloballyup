b759775405eeaec1abf5d1a41578cb836f1010e5
vincentmanono <vincentnyamongo95@gmail.com>
2020-08-02 11:45:59 +0300

    new

--- app/Http/Middleware/ShouldVote.php
+++ app/Http/Middleware/ShouldVote.php
@@ -15,10 +15,8 @@ class ShouldVote
      */
     public function handle($request, Closure $next)
     {
-<<<<<<< HEAD
+
         // $user->wallet->amount
         // return $next($request);
-=======
->>>>>>> 18ecd5a562bd7cf26e7b10d33dd55566109aeaf0
     }
 }
